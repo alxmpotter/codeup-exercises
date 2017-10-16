d22effe0f52d68286b1b67fc4e5d890f6b8a295c
alxmpotter <alxmpotter@gmail.com>
2017-10-16 10:21:31 -0500

    *edit gitignore and new class file

--- src/main/java/com/codeup/FlipItAndReverseIt.java
+++ src/main/java/com/codeup/FlipItAndReverseIt.java
@@ -1,6 +1,8 @@
 package com.codeup;
 
-package org.apache.commons.lang3
+package org.apache.commons.lang3.StringUtils;
+
+import java.lang.Object.StringUtils;
 
 public class FlipItAndReverseIt {
     public static void main(String[] args){
